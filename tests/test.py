462c36d6beb38dac87573c9c14c9c64dfda60fdf
Spartanlasergun <spartanlasergun@outlook.com>
2023-04-21 20:45:00 -0400

    updated testing files

--- tests/test.py
+++ tests/test.py
@@ -8,6 +8,13 @@ from Calendar_Widget import Calendar
 root = tkinter.Tk()
 root.geometry('600x600')
 
+def Calendar_Click():
+	print(Calendar.getdate())
+
+
 Calendar = Calendar(root)
+
+Calendar.checkboxes(8, 4, 2023, status=True)
+Calendar.checkboxes(31, 3, 2023, status=False)
 
 root.mainloop()
