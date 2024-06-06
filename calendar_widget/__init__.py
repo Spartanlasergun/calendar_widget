17e93480e6bdba6d8f604598c4d5592bbe06527b
Spartanlasergun <spartanlasergun@outlook.com>
2024-06-06 08:04:32 -0400

    restructured for workflow optimization

---
+++ calendar_widget/__init__.py
@@ -0,0 +1,1 @@
+from calendar_widget import Calendar
---
+++ tests/import_test.py
@@ -0,0 +1,20 @@
+import tkinter
+import calendar_widget
+
+# define the main window into which the widget will be placed
+root = tkinter.Tk()
+root.geometry('600x600')  # the geometry function defines the size of the tkinter window
+                          # - in this case, we are using a window that is 600px by 600px
+
+# create the calendar widget
+Calendar = Calendar(root, # specify the tkinter window into which the widget will be placed
+	pos_x = 0, # set the x position of the calendar widget within the tkinter window
+	pos_y = 0, # set the y position of the calendar widget within the tkinter window
+	background = 'lightblue', # set the background of the calendar to light blue
+	)
+
+# Note: images of the type 'png', 'gif', 'ppm', and 'pgm' can be set as the background.
+# However, these images will not scale with the size of the calendar.
+
+# remember to call your mainloop function so that the tkinter window is persistent
+root.mainloop()
